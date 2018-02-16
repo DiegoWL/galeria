2c80d89cf0485f1e22820f535281ab122c946d46
Diego Wilson <dwilsonlow@gmail.com>
2018-02-15 22:53:07 -0300

    Primer commit: Pantalla principal implementada, migraciones y controladores creados

---
+++ resources/views/app/inicio.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<!-- saved from url=(0049)https://getbootstrap.com/docs/4.0/examples/album/ -->
+<html lang="en">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://getbootstrap.com/favicon.ico">
+
+    <title>Galeria de Imagenes</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/estilos.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-danger text-white" role="navigation">
+          <a href="https://wwww.karateshotokan.cl" class="navbar-brand d-flex align-items-center text-center">
+            <img src="/img/logodojo.png" class="logo" alt="">
+          </a>
+          <div  class="text-white"  id="navbarNav">
+            <ul class="navbar-nav text-white">
+               <li class="nav-item">
+                 <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="https://wwww.karateshotokan.cl">Volver a Karateshotokan.cl</a>
+               </li>
+            </ul>
+          </div>
+        </nav>
+
+
+    </header>
+     <section class="jumbotron text-center">
+          <div class="container">
+            <h1 class="jumbotron-heading">Galeria de Album</h1>
+            <p class="lead text-muted">En esta sección podrá ver todas las fotos correspondiente a torneos , seminarios y otras actividades.</p>
+            <p>
+              <a href="https://getbootstrap.com/docs/4.0/examples/album/#" class="btn btn-primary my-2">Main call to action</a>
+              <a href="https://getbootstrap.com/docs/4.0/examples/album/#" class="btn btn-secondary my-2">Secondary action</a>
+            </p>
+          </div>
+      </section>
+      <main role="main">
+          @yield('content')
+      </main>
+    <footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+          <a href="https://getbootstrap.com/docs/4.0/examples/album/#">Back to top</a>
+        </p>
+        <p>Album example is © Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="https://getbootstrap.com/docs/4.0/">Visit the homepage</a> or read our <a href="https://getbootstrap.com/docs/4.0/getting-started/">getting started guide</a>.</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="components/jquery/dist/jquery.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="/components/jquery/dist/jquery.slim.min.js"><\/script>')</script>
+    <script src="components/popper.js/dist/popper.min.js"></script>
+    <script src="components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./Album example for Bootstrap_files/holder.min.js.descarga"></script>
+
+
+<svg xmlns="http://www.w3.org/2000/svg" width="348" height="225" viewBox="0 0 348 225" preserveAspectRatio="none" style="display: none; visibility: hidden; position: absolute; top: -100%; left: -100%;"><defs><style type="text/css"></style></defs><text x="0" y="17" style="font-weight:bold;font-size:17pt;font-family:Arial, Helvetica, Open Sans, sans-serif">Thumbnail</text></svg>
+</body>
+</html>
